c1a06937ca5a4e864a80b7f510995b94733653e7
ivan <vanek-3333@ya.ru>
2022-11-03 15:56:33 +0300

    git commit-m"homework_8"

--- homework_8/script.js
+++ homework_8/script.js
@@ -1,85 +1,79 @@
 'use strict';
 
+const button = document.getElementById('submit');
+const resultform = document.getElementById('resultform');
 
-let a = ''; // первое число
-let b = ''; // второе число
-let sign = ''; // знак операции
-let finish = false;
+//button.addEventListener('click', (event) => {
 
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const action = ['-', '+', 'X', '/'];
+function func() {
+    const num1 = document.getElementById('num1').value;
+    const operator = document.getElementById('operator').value;
+    const num2 = document.getElementById('num2').value;
+    let result;
 
-// экран 
-const out = document.querySelector('.calc-screen p');
+    // event.preventDefault();
 
-function clearAll() {
-    a = ''; // первое число и результат
-    b = ''; // второй номер и результат
-    sign = ''; // знак операции
-    finish = false; //сброс чисел
-    out.textContent = 0; //сброс до 0
-}
-    document.querySelector('.ac').onclick = clearAll; //очистка чисел
-
-    document.querySelector('.buttons').onclick = (event) => { //работа по клику
-
-        if (!event.target.classList.contains('btn')) return; // нажата не кнопка
-
-        if (event.target.classList.contains('ac')) return; // нажата кнопка clearAll ac
-
-        out.textContent = '';
-
-        const key = event.target.textContent; // получаю нажатую кнопку
-
-        if (digit.includes(key)) { // если нажата клавиша 0-9 или .
-            if (b === '' && sign === '') {
-                a += key;
-
-                out.textContent = a; //вывод переменной a;
-            } else if (a !== '' && b !== '' && finish) {
-                b = key;
-                finish = false;
-                out.textContent = b; //вывод переменной b;
-            } else {
-                b += key;
-                out.textContent = b; //вывод переменной b;
-            }
-            console.table(a, b, sign); //вывод в console и возврат значений чисел;
-            return;
-        }
-
-        if (action.includes(key)) { // если нажата клавиша + - / x;
-            sign = key; //введен знак операции;
-            out.textContent = sign; //вывод знака операции;
-            console.table(a, b, sign); //вывод в console знака операции;
-            return; //возврат итоговых значений операции;
-        }
-
-        if (key === '=') { // нажата =;
-            if (b === '') b = a;
-            switch (sign) {
-                case "+":
-                    a = (+a) + (+b);
-                    break;
-                case "-":
-                    a = a - b;
-                    break;
-                case "X":
-                    a = a * b;
-                    break;
-                case "/":
-                    if (b === '0') {
-                        out.textContent = 'Ошибка';
-                        a = '';
-                        b = '';
-                        sign = '';
-                        return;
-                    }
-                    a = a / b;
-                    break;
-            }
-            finish = true;
-            out.textContent = a; //вывод переменной a;
-            console.table(a, b, sign); //вывод в console значений операций;
-        }
+    if (num1 === "") {
+        resultform.innerText = "Первое число не указано";
+        return console.log("Первое число не указано");
     }
+
+    if (num2 === "") {
+        resultform.innerText = "Второе число не указано";
+        return console.log("Второе число не указано");
+    }
+
+    if (num1 === " ") {
+        resultform.innerText = "Первое число не указано";
+        return console.log("Первое число не указано");
+    }
+
+    if (num2 === " ") {
+        resultform.innerText = "Второе число не указано";
+        return console.log("Второе число не указано");
+    }
+
+    if (isNaN(num1) === true || isNaN(num2) === true) {
+        resultform.innerText = "Некорректный ввод чисел";
+        return console.log("Некорректный ввод чисел");
+    }
+
+    if (operator === "") {
+        resultform.innerText = "Не введён знак";
+        return console.log("Не введён знак");
+    }
+
+    switch (operator) {
+        case "":
+            return console.log("Не введён знак");
+            break;
+        case '+':
+            result = Number(num1) + Number(num2);
+            break;
+        case '-':
+            result = Number(num1) - Number(num2);
+            break;
+        case '/':
+            result = Number(num1) / Number(num2);
+            break;
+        case '*':
+            result = Number(num1) * Number(num2);
+            break;
+        default:
+            return resultform.innerText = "Программа не поддерживает такую операцию";
+    }
+
+    if (isNaN(result)) {
+        resultform.innerText = "Некорректный ввод чисел";
+        return console.log("Некорректный ввод чисел");
+    }
+
+    if (result === -Infinity || result === Infinity) {
+        resultform.innerText = "Операция некорректна";
+        console.log("Операция некорректна");
+    } else {
+        resultform.innerText = result;
+        console.log(result);
+    }
+    //})
+}
