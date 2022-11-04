8f28d76beb7d3299c7d28e247de0b8405bea446b
ivan <vanek-3333@ya.ru>
2022-11-04 22:32:05 +0300

    git commit-m"homework_9"

--- homework_9/script.js
+++ homework_9/script.js
@@ -25,3 +25,30 @@ fibonacci1(); //  5
 fibonacci1(); //  8
 fibonacci1(); //  13
 fibonacci1(); //  21
+
+const fibonacci2 = (function makeFibonacciFunction() {
+    let next = 1;
+    let last = -1;
+
+    return function () {
+        const result = last + next;
+        last = next;
+        next = result;
+        console.log(result);
+        return result;
+    };
+})();
+
+fibonacci2(); //  0
+fibonacci2(); //  1
+fibonacci2(); //  1
+fibonacci2(); //  2
+fibonacci2(); //  3
+fibonacci2(); //  5
+fibonacci2(); //  8
+fibonacci2(); //  13
+fibonacci2(); //  21
+
+// подробные значения функции со звездочкой *
+// console.log(`last.${last}`);
+// console.log(`next.${ next}`);
