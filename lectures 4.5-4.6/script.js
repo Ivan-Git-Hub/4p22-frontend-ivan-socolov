b19d6037dc9829a9978983ce1d245ea221111f38
ivan <vanek-3333@ya.ru>
2022-11-07 18:42:13 +0300

    git commit-m"lectures_4.5-4.6"

---
+++ lectures 4.5-4.6/script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+
+// function deleteFroArray(array = [], index = 0) {
+//     return array.filter((item, innerIndex) => {
+//         // return !(index === InnerIndex);
+//         return index !== innerIndex;
+//     });
+// }
+
+// function deleteFroArray(array = [], index = 0) {
+//     // return !(index === InnerIndex);
+//     return array.filter((item, innerIndex) => index !== innerIndex);
+// }
+// const arr = ['ghfgh', 'gfhgfh', 'fghfgh'];
+// const obj = {
+//     field: 'fgfg',
+//     field: 'fghfgh',
+//     field: 'dfghfgh',
+// }
+
+// const chelseaSquad = [{
+//     firstName: 'Kepa',
+//     lastName: 'Arrizabalaga',
+// } {
+//     firstName: 'Reece',
+//     lastName: 'James',
+// } {
+//     firstName: 'Cesar',
+//     lastName: 'Azpilicueta',
+// }];
+// const filteredArray = chealseaSquad.filter((item) => {
+//     /*const filteredArray = chealseaSquad.filter((item, index, array) => { */
+//     return item.lastName[0] === 'A';
+//     // return false;
+// });
+
+// // const changedArray = deleteFromArray(chealseaSquad, 1);
+// const changedArray = chelseaSquad.map((item) => {
+//     return {
+//         name: item.firstName
+//     };
+
+//     /*  {
+//         ...item,
+//         id: index
+//     }; */
+
+//     /* {
+//         firstName: item.firstName,
+//         lastName: item.lastName,
+//         id: index
+//     }; */
+
+//     /*  {
+//         name: `${item.firstName} ${item.lastName}`
+//     } */
+//     /*  {
+//             name: item.firstName,
+//             surName: item.lastName,
+//          */
+// }
+// });
+// console.log(filteredArray);
+
+const chelseaSquad = [{
+    firstName: 'Kepa',
+    lastName: 'Arrizabalaga',
+} {
+    firstName: 'Reece',
+    lastName: 'James',
+} {
+    firstName: 'Cesar',
+    lastName: 'Azpilicueta',
+}];
+
+const reducedArray = chelseaSquad.reduce((accumulator, item, index) => {
+        return accumulator;
+    }
+    '');
+const reducedArray = chelseaSquad.reduce((accumulator, item, index) => {
+        return accumulator;
+    }
+    '');
+const reducedArray = chelseaSquad.reduce((accumulator, item, index) => {
+        return accumulator;
+    }
+    '');
